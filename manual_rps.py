abead7e162c79148ca8ae841f7bb8ab8ebd6b5c1
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-14 22:54:21 +0000

    Within get_winner() replaced & with and for checks

--- manual_rps.py
+++ manual_rps.py
@@ -9,21 +9,21 @@ def get_user_choice():
     return user_choice
 
 def get_winner(computer_choice, user_choice):
-    if get_computer_choice() == 'Rock' and get_user_choice() == 'Rock':
+    if computer_choice == 'Rock' and user_choice == 'Rock':
         print("It is a tie!")
-    elif get_computer_choice() == 'Rock' and get_user_choice() == 'Scissors':
+    elif computer_choice == 'Rock' and user_choice == 'Scissors':
         print("You lost")
-    elif get_computer_choice() == 'Rock' and get_user_choice() == 'Paper':
+    elif computer_choice == 'Rock' and user_choice == 'Paper':
         print("You won!")
-    elif get_computer_choice() == 'Paper' and get_user_choice() == 'Rock':
+    elif computer_choice == 'Paper' and user_choice == 'Rock':
         print("You lost")
-    elif get_computer_choice() == 'Paper' and get_user_choice() == 'Scissors':
+    elif computer_choice == 'Paper' and user_choice == 'Scissors':
         print("You won!")
-    elif get_computer_choice() == 'Paper' and get_user_choice() == 'Paper':
+    elif computer_choice == 'Paper' and user_choice == 'Paper':
         print("It is a tie!")
-    elif get_computer_choice() == 'Scissors' and get_user_choice() == 'Paper':
+    elif computer_choice == 'Scissors' and user_choice == 'Paper':
         print("You lost")
-    elif get_computer_choice() == 'Scissors' and get_user_choice() == 'Rock':
+    elif computer_choice == 'Scissors' and user_choice == 'Rock':
         print("You won!")
     else:
         print("It is a tie")
