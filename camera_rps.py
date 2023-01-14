4919dfee4cebdac2df63ccd55f9a0f3977a616a7
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-14 23:43:15 +0000

    Added timer

--- camera_rps.py
+++ camera_rps.py
@@ -1,6 +1,5 @@
 import cv2
 import time
-time_start = time.time()
 from keras.models import load_model
 import numpy as np
 
@@ -8,10 +7,22 @@ model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-while True:
-    if time.time() - time_start > 5:
-        print("you chose rock")
-        def get_prediction():
-            return(model.predict(data))
-        break
+def get_prediction():
+    return(model.predict(data))
+        
 
+while True: 
+    time_start = time.time()
+    if time.time() - time_start > 5:
+        ret, frame = cap.read()
+        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+        image_np = np.array(resized_frame)
+        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+        data[0] = normalized_image
+        
+        cv2.imshow('frame', frame)
+        # Press q to close the window
+        print(model.predict(data))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        get_prediction()
