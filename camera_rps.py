6aa0661fecdca7a389a4d8631185eacae2b36b0a
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-14 23:38:17 +0000

    Added timer

--- camera_rps.py
+++ camera_rps.py
@@ -1,31 +1,17 @@
 import cv2
+import time
+time_start = time.time()
 from keras.models import load_model
 import numpy as np
+
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-"""
-while True: 
-    ret, frame = cap.read()
-    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-    image_np = np.array(resized_frame)
-    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-    data[0] = normalized_image
-    prediction = model.predict(data)
-    cv2.imshow('frame', frame)
-    # Press q to close the window
-    print(prediction)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+while True:
+    if time.time() - time_start > 5:
+        print("you chose rock")
+        def get_prediction():
+            return(model.predict(data))
         break
-            
-# After the loop release the cap object
-cap.release()
-# Destroy all the windows
-cv2.destroyAllWindows()
-"""
-
-def get_prediction():
-    return(model.predict(data))
-
 
